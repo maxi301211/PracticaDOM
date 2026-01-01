1de2df530d372a4745b56b503344aedf8992cf3d
maxicajal <mcajalborquez@gmail.com>
2026-01-01 19:32:40 -0300

    modifique el titulo de la segunda card para ocultarlo al presionar un boton

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const modificarTitulo = () => {
-    const titulo = document.getElementById('titulo')
-    titulo.textContent = 'Nuevo título'
-    titulo.classList.add('display-6')
-    console.log(titulo)
-}
+  const titulo = document.getElementById("titulo");
+  titulo.textContent = "Nuevo título";
+  titulo.classList.add("display-6");
+  console.log(titulo);
+};
 
 const modificarTituloConInnerHTML = () => {
-    const titulo = document.getElementById('titulo')
-    titulo.innerHTML = 'Texto usaando <b>innerHTML</>'
-}
+  const titulo = document.getElementById("titulo");
+  titulo.innerHTML = "Texto usaando <b>innerHTML</b>";
+};
 
-const btnCambiartituloConinnerHTML = document.querySelector('#btnCambiarTituloConInnerHTML')
+const ocultarTexto = () => {
+  const titulos = document.getElementsByClassName("card-title");
+  const btn = document.getElementById("btnOcultarTexto");
 
-// agregar un manejador de eventos 
-btnCambiartituloConinnerHTML.addEventListener('click', modificarTituloConInnerHTML)
+  titulos[1].classList.toggle("d-none");
 
+  btn.textContent = titulos[1].classList.contains("d-none") 
+    ? "Mostrar texto" 
+    : "Ocultar texto";
+};
+
+const btnCambiartituloConinnerHTML = document.querySelector(
+  "#btnCambiarTituloConInnerHTML"
+);
+
+// agregar un manejador de eventos
+btnCambiartituloConinnerHTML.addEventListener(
+  "click",
+  modificarTituloConInnerHTML
+);
