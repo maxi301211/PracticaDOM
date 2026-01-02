483d16c88f059040dab04a21a1112303b9be1340
maxicajal <mcajalborquez@gmail.com>
2026-01-02 16:59:58 -0300

    modifique un boton para hacer que elimine el titulo

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const modificarTitulo = () => {
   const titulo = document.getElementById("titulo");
   titulo.textContent = "Nuevo título";
   titulo.classList.add("display-6");
-  console.log(titulo);
 };
 
 const modificarTituloConInnerHTML = () => {
@@ -16,17 +15,29 @@ const ocultarTexto = () => {
 
   titulos[1].classList.toggle("d-none");
 
-  btn.textContent = titulos[1].classList.contains("d-none") 
-    ? "Mostrar texto" 
+  btn.textContent = titulos[1].classList.contains("d-none")
+    ? "Mostrar texto"
     : "Ocultar texto";
+};
+
+const borrarTitulo = () => {
+  const titulos = document.getElementsByClassName("card-title");
+  const titulo = titulos[2];
+  if (titulo) {
+    titulo.remove();
+    btnBorrarTitulo.classList.add('disaled')
+  }
 };
 
 const btnCambiartituloConinnerHTML = document.querySelector(
   "#btnCambiarTituloConInnerHTML"
 );
+const btnBorrarTitulo = document.getElementById("btnBorrarTitulo");
 
 // agregar un manejador de eventos
 btnCambiartituloConinnerHTML.addEventListener(
   "click",
   modificarTituloConInnerHTML
 );
+
+btnBorrarTitulo.addEventListener("click", borrarTitulo);
